28c88229ce649f5a8d0aea848bc20481b4d0854b
jesp6763 <jesp6763@edu.campusvejle.dk>
2018-06-26 09:48:41 +0200

    Refactored, and changed the price to be a number instead of string

--- js/Model/Product.js
+++ js/Model/Product.js
@@ -12,7 +12,7 @@ class Product {
         this.id = id;
         this.name = info.name;
         this.description = info.description;
-        this.price = info.price;
+        this.price = Number(info.price.slice(0, -2));
         this.img = info.img;
     }
 
@@ -39,7 +39,7 @@ class Product {
             creationCompleteCallback();
             console.log("Test data created");
 
-            Product.SaveAllProductInstances();
+            Product.SaveAll();
         });
 
     }
@@ -47,23 +47,9 @@ class Product {
     /**
      * Saves the Product.Instances to the local storage.
      */
-    static SaveAllProductInstances() {
-        this.SaveAll(Product.Instances, "localstorage");
-    }
-
-    /**
-     * Saves the instances in the specified storage.
-     * @param {Product[]} instances An array of Product instances.
-     * @param {string} method The saving method. Acceptable values: "localStorage", "session".
-     */
-    static SaveAll(instances, method) {
+    static SaveAll() {
         try {
-            if (method.toLowerCase() === "localstorage") {
-                this._SetProductLocalStorage(JSON.stringify(instances));
-            }
-            else if (method.toLowerCase() === "session") {
-                this._SetProductSessionStorage(JSON.stringify(instances));
-            }
+            Storage.Local.set.ProductList(JSON.stringify(Product.Instances));
         }
         catch (error) {
             alert('Failed to read from localstorage.\n\n' + error);
@@ -71,52 +57,13 @@ class Product {
     }
 
     static LoadAll(loadCompleteCallback) {
-        if (this._GetProductLocalStorage()) {
-            Product.Instances = JSON.parse(this._GetProductLocalStorage());
+        if (Storage.Local.get.ProductList) {
+            Product.Instances = JSON.parse(Storage.Local.get.ProductList);
             loadCompleteCallback();
         }
         else {
             this.CreateTestData(loadCompleteCallback);
         }
-    }
-
-    /**
-     * Gets the value stored in StarbuckProducts local storage.
-     */
-    static _GetProductLocalStorage() {
-        return localStorage['StarbucksProducts'];
-    }
-
-    /**
-     * Gets the value stored in StarbuckProducts session storage.
-     */
-    static _GetProductSessionStorage() {
-        return sessionStorage['StarbucksProducts'];
-    }
-
-    /**
-     * Sets the product local storage to a new value.
-     * @param {string} newValue The new StarbuckProducts local storage value.
-     */
-    static _SetProductLocalStorage(newValue) {
-        localStorage['StarbucksProducts'] = newValue;
-    }
-
-    /**
-     * Sets the product session storage to a new value.
-     * @param {string} newValue The new StarbuckProducts session storage value.
-     */
-    static _SetProductSessionStorage(newValue) {
-        sessionStorage['StarbucksProducts'] = newValue;
-    }
-
-    /**
-     * Stores a value at the specified key in Product.Instances.
-     * @param {string} key The key of where the value will be stored.
-     * @param {Product} newValue The value where the specified key is.
-     */
-    static _SetProductItemValue(key, newValue) {
-        Product.Instances[key] = newValue;
     }
 }
 
